6a95df61a1faa1441560f3e3f8424328c8aa4fa7
Nicolas Diaz <nickmusik3001@gmail.com>
2021-10-25 20:56:00 -0300

    primer commit

---
+++ Assets/Scripts/CannonController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("ShootCannon",2f,1.5f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            for(var i = 0; i < 2; i++)
+            {
+                Debug.Log("Shoot Bullet number: "+i);
+                ShootCannon();
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.K)){
+            for(var i = 0; i < 3; i++)
+            {
+                Debug.Log("Shoot Bullet number: "+i);
+                ShootCannon();
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.L))
+        {
+          for(var i = 0; i < 4; i++)
+            {
+                Debug.Log("Shoot Bullet number: "+i);
+                ShootCannon();
+            }  
+        }}
+
+    void ShootCannon()
+    {
+        Instantiate(bulletPrefab,transform);
+    }
+}
